c2d9a7a84695b4e2aa85fd8d34ca32f063205f67
Rao Muhammad Akif Tufail <akifrao@gmail.com>
2024-12-21 17:27:37 +0500

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    domains: ["dummyimage.com"], // Add dummyimage.com here
+  },
 };
 
 export default nextConfig;
